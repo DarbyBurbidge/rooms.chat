fc345da3d33e33c0506c15880aaf66c44fc62477
DarbyBurbidge <darby.burbidge@gmail.com>
2024-04-29 19:10:40 -0700

    mondb atlas connecting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,39 @@
-console.log("Hello World!!!")
+import * as http from "http";
+import * as express from "express";
+import * as cors from "cors";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+
+require("dotenv").config();
+console.log(process.env.ATLAS_URI)
+
+const app = express.default();
+const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+
+const corsOptions = {
+	origin: '*',
+	transports: ['websocket', 'polling']
+};
+
+app.use(cors.default(corsOptions));
+
+var db = mongoose.connection;
+
+db.on('error', async (err) => {
+	console.error(err);
+});
+db.once('open', async () => {
+	console.log("connected to Atlas");
+});
+
+mongoose.connect(`${process.env.ATLAS_URI}`);
+
+io.on("connection", (socket) => {
+
+})
+server.listen(port, () => {
+	console.log(`connected on port ${port}`);
+});
+
