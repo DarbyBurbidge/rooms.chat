143f7278c38278ee1c7af92b2bb9d56197c98079
mackenzie-norman <115865992+mackenzie-norman@users.noreply.github.com>
2024-05-13 23:39:27 -0700

    working on chat

--- frontend/src/chat.tsx
+++ frontend/src/chat.tsx
@@ -1,29 +1,52 @@
 import { Component } from 'react';
-import { Container, Stack } from 'react-bootstrap';
+import { Button, Container, Row, Stack } from 'react-bootstrap';
 import { MessageBox } from "react-chat-elements";
+
+import Card from 'react-bootstrap/Card'
+import { CardBody, CardHeader } from 'react-bootstrap';
+import { Input } from 'react-chat-elements' 
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 import RoomModel from '../../backend/src/models/room.ts'
 
 
 class ChatRoom extends Component{
+    input: string;
 
-    constructor(props) {
+ constructor(props) {
     super(props);
     this.state = {
         msgs: ["Test1", "Test2" ,"Test3"],
         room_name: "test Room"
 
     }
+    this.input = "";
   }
+
+  add_msg(){
+    text = this.input
+    this.state.msgs.append(text)
+    this.setState({msgs: this.state.msgs.append(text)})
+}
+
   render() {
-    const { Rooms } = this.state;
+    const {msgs} = this.state;
     return (
         <>
-        <Container>
-            <MessageBox position={"left"} type={"text"} title={"Message Box Title"} text="Here is a text type message box">  </MessageBox> 
+        <br></br>
+        <Card>
+            <CardHeader>{this.state.room_name}</CardHeader>
+        <CardBody>
+        <Row>
+            {msgs.map((room, index) => (
+            <MessageBox position={"left"} type={"text"} title={room} text ={room}>  </MessageBox> 
+            ))}
             <MessageBox position={"right"} type={"text"} title={"Message Box Title"} text="Here is a text type message box">  </MessageBox> 
-        </Container>
+    </Row>
+        <Row>
+        <Input ref={this.input} placeholder="Type here..."multiline={true}  rightButtons=<Button onClick={this.add_msg} >Send</Button> /> </Row>
+        </CardBody>
+        </Card>
 </>
   );
 }
