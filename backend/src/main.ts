1c66ce8645562278a926e2677588fc2168d5d712
DarbyBurbidge <darby.burbidge@gmail.com>
2024-05-28 10:17:03 -0700

    added websocket link that joins new client connections to sockets associated with the user so they can appear persistent

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose"
 import { Server } from "socket.io";
 import { config } from "dotenv";
+import { OAuth2Client, LoginTicket } from "google-auth-library";
 import { accountRouter } from "./routes/account.ts";
 import { contactRouter } from "./routes/contact.ts";
 import { roomRouter } from "./routes/room.ts";
@@ -45,11 +46,29 @@ app.use("/note", noteRouter);
 // message
 app.use("/message", messageRouter);
 
-io.on("connection", (socket) => {
-	console.log(`connected to socket ${socket.id}`);
-	socket.rooms.forEach((room) => {
-		console.log(`room is: ${room}`);
-	});
+io.on("connection", async (socket) => {
+	try {
+		console.log(`connected to socket ${socket.id}`);
+		socket.rooms.forEach((room) => {
+			console.log(`room is: ${room}`);
+		});
+		const cookie = socket.client.request.headers.cookie;
+		console.log(cookie)
+		const token = decodeURI(cookie!).split('Authorization=Bearer ')[1];
+		console.log(`authToken: ${cookie}`);
+		const redirectUrl = 'http://localhost:3000/oauth';
+		const oAuth2Client = new OAuth2Client(
+			process.env.CLIENT_ID,
+			process.env.CLIENT_SECRET,
+			redirectUrl
+		);
+		const ticket: LoginTicket = await oAuth2Client.verifyIdToken({ idToken: token, audience: process.env.CLIENT_ID });
+		console.log(ticket);
+		socket.join(ticket.getPayload()!.sub);
+	} catch (err) {
+		console.error(err)
+		socket.emit("Could not link socket to user")
+	}
 });
 io.on("error", (err) => {
 	console.error(err);
